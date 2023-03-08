ee1c213c8ca43afac95e65dc0a98ff9801a2fa70
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-03-08 15:39:29 -0500

    connected css file to main

--- routes/api/addCard.js
+++ routes/api/addCard.js
@@ -1,4 +1,13 @@
 const router = require("express").Router();
 const Card = require("../../models/Card");
 
+router.post("/", async (req, res) => {
+  try {
+    const newCard = await Card.create(req.body);
+    res.render("addCard", newCard);
+  } catch (err) {
+    res.status(500).send("Could not add card to database", err);
+  }
+});
+
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-
+const addCard = require("./addCard");
 const userRoutes = require("./userRoutes");
 
 router.use("/cards", userRoutes);
+router.use("/add", addCard);
 
 module.exports = router;
